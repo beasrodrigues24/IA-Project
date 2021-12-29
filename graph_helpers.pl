ce47506f279fac16dfd9787c38109845fd52f52f
Parola <parolajc@hotmail.com>
2021-12-29 11:45:33 +0000

    start Gulosa search

--- graph_helpers.pl
+++ graph_helpers.pl
@@ -28,7 +28,46 @@ larguraAux(Dest,[EstadosA|OutrosEstados],Solucao) :-
 	append(OutrosEstados,Novos,Todos),
 	larguraAux(Dest,Todos,Solucao).
 
+% ---------------------------------------------------------------
+
+% gulosa(Orig,Dest,Caminho)
+
+gulosa(Orig,Dest,CaminhoDistancia,CaminhoTransito) :-
+	estima(Orig,Dest,EstimaD,EstimaT),
+	gulosaAuxDist(Dest,[[Orig]/EstimaD],InvCaminhoDist),
+	gulosaAuxTransito(Dest,[[Orig]/EstimaT],InvCaminhoTransito),
+	reverse(InvCaminhoDist,CaminhoDistancia),
+	reverse(InvCaminhoTransito,CaminhoTransito).
+
+gulosaAuxDist(Dest,Caminhos,Caminho) :-
+	obtem_melhor_g(Caminhos,Caminho),
+	Caminho = [Dest|]/_.
+gulosaAuxDist(Dest,Caminhos,SolucaoCaminho) :-
+	obtem_melhor_g(Caminhos,MelhorCaminho),
+	seleciona(MelhorCaminho,Caminhos,OutrosCaminhos),
+	expande_gulosa(MelhorCaminho,ExpCaminhos),
+
+
+obtem_melhor_g([Caminho],Caminho) :- !.
+obtem_melhor_g([Caminho1/EstimaD1,_/EstimaD2|Caminhos],MelhorCaminho) :- 
+	EstimaD1 =< EstimaD2,
+	!,
+	obtem_melhor_g([Caminho1/EstimaD1|Caminhos],MelhorCaminho).
+
+obtem_melhor_g([_|Caminhos],MelhorCaminho) :-
+	obtem_melhor_g(Caminhos,MelhorCaminho).
+
+% TODO
+%expande_gulosa(Caminho,ExpCaminhos) :-
+%	findall(NovoCaminho,(
+%			adjacente(Caminho,NovoCaminho),
+%
+%		)
+%		,ExpCaminhos).
 % Graph helpers
 
 adjacente(Nodo,ProxNodo) :- edge(Nodo,ProxNodo,_,_).
 adjacente(Nodo,ProxNodo) :- edge(ProxNodo,Nodo,_,_).
+
+seleciona(E,[E|Xs],Xs).
+seleciona(E,[X|Xs],[X|Ys]) :- seleciona(E,Xs,Ys).
